dde97b9b4e5ebe2e731399700bb46c7fde47c6d0
Ajay Wakodikar <ajaywakodikar10@gmail.com>
2024-03-07 16:39:40 +0530

    Added the lexer object to tokenise the string and display the kind of tokens

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include "lex.h"
 
 int main() {
     std::string line;
@@ -8,6 +9,17 @@ int main() {
     while(true) {
         std::cout << "->: ";
         std::getline(std::cin, line);
+        //Lexer object to tokenise the string
+        Lexer* lex = new Lexer(line);
+	Syntax_Token* token;
+
+	while(true) {
+		token = lex->next_token();
+
+		if(token->_kind == Token_Kind::end_of_file_token) break;
+
+		std::cout << token_kind_to_string(token) << ": \'" << token->_text << "\'\n";
+	}
 
         //Return if empty
         if(line.empty()) {
